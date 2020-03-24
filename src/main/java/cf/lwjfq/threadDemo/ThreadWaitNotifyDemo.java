0db4960bab2215c6a26ed636db93ac197c250f41
liwenjie001 <1554616416@qq.com>
2020-03-24 17:46:06 +0800

    '添加一个新锁的方法'

--- src/main/java/cf/lwjfq/threadDemo/ThreadWaitNotifyDemo.java
+++ src/main/java/cf/lwjfq/threadDemo/ThreadWaitNotifyDemo.java
@@ -1,5 +1,8 @@
 package cf.lwjfq.threadDemo;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @ Author: Mr.Li
  * @ Date: 2020-03-24 15:56
@@ -7,15 +10,15 @@ package cf.lwjfq.threadDemo;
  * 题目：现在有两个线程，可以操作初始值为零的变量，实现一个线程对变量加1，另一个线程对变量减1.
  * 实现交替来10个轮回。变量值为0；
  * 思想：
- *  1. 高聚低合前提下，线程操作资源类
- *  2. 判断//干货//通知
- *  3. 多线程交互下，必须防止多线程的虚假唤醒，也即（判断使用while 不能使用if）
+ * 1. 高聚低合前提下，线程操作资源类
+ * 2. 判断//干货//通知
+ * 3. 多线程交互下，必须防止多线程的虚假唤醒，也即（判断使用while 不能使用if）
  **/
 public class ThreadWaitNotifyDemo {
     public static void main(String[] args) {
         AirConditioner airConditioner = new AirConditioner();
 
-        new Thread(()->{
+        new Thread(() -> {
             for (int i = 0; i < 10; i++) {
                 try {
                     airConditioner.increment();
@@ -24,8 +27,8 @@ public class ThreadWaitNotifyDemo {
                 }
             }
 
-        },"A").start();
-        new Thread(()->{
+        }, "A").start();
+        new Thread(() -> {
             for (int i = 0; i < 10; i++) {
                 try {
                     airConditioner.decrement();
@@ -33,30 +36,66 @@ public class ThreadWaitNotifyDemo {
                     e.printStackTrace();
                 }
             }
-        },"B").start();
+        }, "B").start();
     }
 }
 
-class AirConditioner{
+// 老版本的ynchronized 的实现方式
+class AirConditioner1 {
     private int number = 0;
+
     public synchronized void increment() throws InterruptedException {
         // 判断
-        while (number!=0){
+        while (number != 0) {
             this.wait();
         }
         // 干货
         number++;
         // 通知
         this.notifyAll();
-        System.out.println(Thread.currentThread().getName()+":"+number);
+        System.out.println(Thread.currentThread().getName() + ":" + number);
 
     }
+
     public synchronized void decrement() throws InterruptedException {
-        while (number==0){
+        while (number == 0) {
             this.wait();
         }
         number--;
         this.notifyAll();
-        System.out.println(Thread.currentThread().getName()+":"+number);
+        System.out.println(Thread.currentThread().getName() + ":" + number);
+    }
+}
+
+// 新版本lock 的实现方式
+class AirConditioner {
+    private int number = 0;
+    private ReentrantLock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
+
+    public void increment() throws InterruptedException {
+
+        lock.lock();
+        // 判断
+        while (number != 0) {
+            condition.await();
+        }
+        // 干货
+        number++;
+        // 通知
+        condition.signalAll();
+        System.out.println(Thread.currentThread().getName() + ":" + number);
+        lock.unlock();
+    }
+
+    public void decrement() throws InterruptedException {
+        lock.lock();
+        while (number == 0) {
+            condition.await();
+        }
+        number--;
+        condition.signalAll();
+        System.out.println(Thread.currentThread().getName() + ":" + number);
+        lock.unlock();
     }
 }
